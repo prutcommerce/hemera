76d23d23f3c81b4c36370984e7768b851e0cb25d
radu <radu.buciuceanu@gmail.com>
2022-07-12 07:44:15 +0300

    [master] Create order db table

---
+++ src/migrate-db/migrations/2022.07.12T04.28.59.create-order-table.js
@@ -0,0 +1,41 @@
+import { sequelize } from 'src/deps'
+
+/**
+ * @param {import('sequelize').QueryInterface} queryInterface
+ */
+export const up = async queryInterface => queryInterface.createTable('order', {
+  id: {
+    type: sequelize.DataTypes.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  created_at: {
+    type: sequelize.DataTypes.DATE,
+    defaultValue: sequelize.literal('UTC_TIMESTAMP'),
+    allowNull: true,
+  },
+  updated_at: {
+    type: sequelize.DataTypes.DATE,
+    defaultValue: sequelize.literal('UTC_TIMESTAMP'),
+    allowNull: true,
+  },
+  deleted_at: {
+    type: 'DATETIME NULL',
+    allowNull: true,
+  },
+  product_id: {
+    type: sequelize.DataTypes.INTEGER.UNSIGNED,
+    references: {
+      model: 'product',
+      key: 'id'
+    },
+    allowNull: false,
+  },
+  quantity: sequelize.DataTypes.FLOAT,
+  status: sequelize.DataTypes.STRING,
+})
+
+/**
+ * @param {import('sequelize').QueryInterface} queryInterface
+ */
+export const down = async queryInterface => queryInterface.dropTable('order')
